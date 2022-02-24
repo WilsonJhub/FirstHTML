2b40e713eb9452f7a0204452e9f18e4226e18658
Jason Wilson <jasonstimphil@gmail.com>
2022-02-23 22:11:44 -0500

    update to FirstHTML

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log 
+let userName = "Beardy";
+console.log(userName);
+
+var myName = prompt("Do you love beards?");
+console.log("myName: ", myName);
+
+if (myName == 'Hexx') {
+    document.write('It is my HONOR to personally welcome you to the Gallery of Beards, ' + myName);
+    } else {
+        document.write("Welcome," + myName);
+    }
+
+// document.write(myName);
+
+
